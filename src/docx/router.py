9f08bd193d2ed50b0b5d35666b9b1ea6eccc5d1b
Mavlin Dm <mavlind@list.ru>
2023-03-15 01:35:45 +0700

    ущу какой-то вариант

--- src/docx/router.py
+++ src/docx/router.py
@@ -1,13 +1,15 @@
+import inspect
 import json
 import pathlib
 import shutil
 from json import JSONDecodeError
-from typing import List, Optional, Annotated
+from typing import List, Optional, Annotated, Generator, Callable, Any, Dict, Type
 
 from fastapi import FastAPI, Depends, File, UploadFile, Form, HTTPException, Body, Header
 from fastapi.encoders import jsonable_encoder
 from logrich.logger_ import log  # noqa
 from pydantic import BaseModel, ValidationError, Field, validator, PositiveInt
+from pydantic.fields import ModelField
 from starlette import status
 from docxtpl import DocxTemplate
 
@@ -176,8 +178,196 @@ class GroupRename(BaseModel):
 
 
 class Model(BaseModel):
-    foo: UploadFile = File(..., exclusiveMaximum=10)
-    # foo: PositiveInt = Field(..., exclusiveMaximum2=10)
+    # file: UploadFile = Body(..., exclusiveMaximum2=10)
+    file: UploadFile
+    # = File(..., description='description222')
+    # @validator("file")
+    # def decode_filenames(cls, val: UploadFile, values: dict) -> UploadFile:
+    #     try:
+    #         if val.size>1:
+    #             log.debug(val)
+    #             return val
+    # #             # return json.loads(val)
+    #     except Exception as e:
+    #         log.trace(e)
+    #         # raise HTTPException(
+    #         #     detail=f'Поле filename невозможно сериализовать: {val}',
+    #         #     status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+    #         # )
+    # @classmethod
+    # def __modify_schema__(
+    #     cls, field_schema: Dict[str, Any], field: Optional[ModelField]
+    # ):
+    #     field_schema['description'] = 'kjhkhkjh'
+    # if field:
+    #     size = field.field_info.extra['size']
+    #     field_schema['examples'] = [c * 3 for c in size]
+
+
+class RestrictedAlphabetStr(UploadFile):
+    # class RestrictedAlphabetStr(str):
+
+    value: UploadFile = File(...)
+
+    # @classmethod
+    # def __get_validators__(cls) -> Generator[Callable, None, None]:
+    #     yield cls.validate
+    #
+    # @classmethod
+    # def validate(cls, value: UploadFile, field: UploadFile):
+    #     size = field.field_info.extra['size']
+    #     log.debug(field)
+    #     log.debug(value)
+    #     log.debug(size)
+    #     if any(c not in size for c in value):
+    #         raise ValueError(f'{value!r} is not restricted to {size!r}')
+    #     return cls(value)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema: Dict[UploadFile, Any], field: Optional[ModelField]):
+        if field:
+            # size = field.field_info.extra['size']
+            field_schema["examples"] = "lijjuojh"
+
+
+class SimpleModel(BaseModel):
+    no: int = Body(...)
+    # nm: str = Form(...)
+    f: UploadFile = Body(..., example="kjhkjh")
+
+    @classmethod
+    def __modify_schema__(cls, field_schema: Dict[UploadFile, Any], field: Optional[ModelField]):
+        if field:
+            # size = field.field_info.extra['size']
+            field_schema["examples"] = "lijjuojh"
+
+
+def as_form(cls: Type[BaseModel]) -> Type[BaseModel]:
+    """
+    Adds an `as_form` class method to decorated models. The `as_form` class
+    method can be used with `FastAPI` endpoints.
+
+    Args:
+        cls: The model class to decorate.
+
+    Returns:
+        The decorated class.
+
+    """
+
+    def make_form_parameter(field: ModelField) -> Any:
+        """
+        Converts a field from a `Pydantic` model to the appropriate `FastAPI`
+        parameter type.
+
+        Args:
+            field: The field to convert.
+
+        Returns:
+            Either the result of `Form`, if the field is not a sub-model, or
+            the result of `Depends` if it is.
+
+        """
+        if issubclass(field.type_, BaseModel):
+            # This is a sub-model.
+            assert hasattr(field.type_, "as_form"), (
+                f"Sub-model class for {field.name} field must be decorated with" f" `as_form` too."
+            )
+            return Depends(field.type_.as_form)
+        else:
+            return Form(field.default) if not field.required else Form(...)
+
+    new_params = [
+        inspect.Parameter(
+            field.alias,
+            inspect.Parameter.POSITIONAL_ONLY,
+            default=make_form_parameter(field),
+        )
+        for field in cls.__fields__.values()
+    ]
+
+    async def _as_form(**data):
+        return cls(**data)
+
+    sig = inspect.signature(_as_form)
+    sig = sig.replace(parameters=new_params)
+    _as_form.__signature__ = sig
+    setattr(cls, "as_form", _as_form)
+    return cls
+
+
+@as_form
+class Nested(BaseModel):
+    foo: int
+    bar: str
+
+
+@as_form
+class Outer(BaseModel):
+    inner: UploadFile = File(...)
+    baz: float
+
+
+# class MyModel:
+class MyModel(BaseModel):
+    # value: RestrictedAlphabetStr = File(size='100', media_type= "multipart/form-data")
+    # f: Annotated[
+    #         UploadFile,
+    #         File
+    # ]
+    #         File(...,description='description')
+    def __init__(self, **data: Any):
+        super().__init__(**data)
+        self.field_schema["description"] = "lijjuojh"
+
+    f: UploadFile = File(..., description="description", media_type="multipart/form-data")
+
+    @classmethod
+    def __modify_schema__(cls, field_schema: Dict[str, Any], field: Optional[ModelField]):
+        # if field:
+        # size = field.field_info.extra['size']
+        cls.field_schema["description"] = "lijjuojh"
+
+    # class Config:
+    #     title = 'Main'
+    # file: Any = File(...)
+    # file: int = Body(..., gt=10000)
+
+
+def valf(content_length):
+    # async def valf(content_length: int = Header(..., lt=80_000)):
+    #     return  File(None,description='description')
+    return Annotated[UploadFile, File(None, description="description")]
+
+
+# async def wrap(q, arg):
+async def common_parameters(q: UploadFile | None = File(..., description=f"jhgjhg")):
+    # return {"q": File(..., description='description') }
+    return {
+        "q": q,
+    }
+    # r = await common_parameters(q=q)
+    # return r
+
+
+class FixedContentQueryChecker:
+    def __init__(self, limit: int = 4):
+        self.limit = limit
+        self.f = File(..., description=f"jhgjhglimit")
+        # log.trace(self.f)
+
+    def __call__(
+        self, q: UploadFile | None = File(..., description=f"jhgjhglimit{config.LOG_LEVEL}")
+    ):
+        # log.debug(self.f)
+        # if q<10:
+        #     return self.limit
+        return {
+            "q": q,
+        }
+
+
+checker = FixedContentQueryChecker(limit=10)
 
 
 @router.post(
@@ -186,21 +376,38 @@ class Model(BaseModel):
     description=f"Требуется аудиенция: **{Audience.UPDATE.value}**",
     # dependencies=[Depends(check_create_access)],
     response_model=set[str],
+    openapi_extra={"limit": 10},
     status_code=status.HTTP_200_OK,
 )
 async def upload_template(
+    # file: BaseModel
+    # file: MyModel=Depends()
+    # file: Model=Depends()
+    fixed_content_included: dict = Depends(checker),
+    # commons: dict = Depends(common_parameters)
+    # commons: UploadFile = Depends(File)
+    # file: Model
+    #     form: Outer = Depends(Outer.as_form)
+    #     form_data: SimpleModel = Depends()
+    # file: bytes = File(description='jhgjgg', examples='jhgjg')
+    # foo: UploadFile = File(...,description='description'),
+    # file: Depends(MyModel)
+    # value: MyModel
     # name: str,
     # model: Base = Depends(base_checker),
     # data: Base = Body(...),
     # data: Base = Depends(Base),
     #     payload: GroupRename,
+    # data: Base2 ,
     # data: Base2 = Depends(),
     #     filename: str = Form(...),
     #     token: str = Form(...),
     # data: Base = Depends(base_checker),
-    # foo: str = Body(...),
+    # foo:RestrictedAlphabetStr,
+    # foo:MyModel,
+    # foo: UploadFile =MyModel,
     # baz: str = Body(...),
-    file: Model,
+    # file: Model,
     # filename: str = Form(
     #     None,
     #     description="Сериализованный список имён файлов, файлы будут сохранены под указанными именами. Если имена не указаны файлы сохранятся как есть.",
@@ -209,7 +416,8 @@ async def upload_template(
     #         str, Field(min_length=10, max_length=100)
     #     ]
     # token: str = Form(...),
-    #     file: UploadFile = File(...),
+    # file: UploadFile = Depends(MyModel),
+    # file: UploadFile = File(...),
     #     file = File(..., gt=10_0),
     #     foo: PositiveInt = Field(..., exclusiveMaximum=10)
     #     file_size: int = Depends(valid_content_length)
