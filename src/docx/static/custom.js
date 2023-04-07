08727dccccd55bc6a2646fb326f50b38003c9eb1
Mavlin Dm <mavlind@list.ru>
2023-04-07 20:24:47 +0700

    upd: ref styles

--- src/docx/static/custom.js
+++ src/docx/static/custom.js
@@ -1,13 +1,28 @@
-// в оиписании путей слеши стилизованы отдельно
+// в описании путей слеши стилизованы отдельно
 function replace_path(paths, paths_data) {
+    var re = new RegExp('^{.*}$')
     for (let i = 0; i < paths.length; i++) {
-        paths[i].innerHTML = paths_data[i].dataset.path.split('/').join('<span class="summary-path slash">/</span>')
+        // console.log(paths_data[i].dataset.path)
+        pathParts = paths_data[i].dataset.path.split('/')
+        let content = ''
+        pathParts.forEach((val, key) => {
+            if (val) {
+                if (re.test(val)) {
+                    val=val.slice(1,-1)
+                    content += `<span class="summary-path slash">/</span><span class="brace">{</span><span class="summary-path param">${val}</span><span class="brace">}</span>`
+                } else {
+                    content += `<span class="summary-path slash">/</span><span>${val}</span>`
+                }
+            }
+        })
+        paths[i].innerHTML = content
     }
 }
 
 window.onload = function () {
 
     (async () => {
+        // console.log('path')
         let paths
         let paths_data
         for (let i = 0; i < 10; i++) {
